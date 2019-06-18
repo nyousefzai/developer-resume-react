b6af58b9ebc4f4cfad51311b5674fc67890b87fa
Navid <navid22y@gmail.com>
2019-06-17 17:26:05 -0700

    lazy load

--- app/components/Portfolio/index.js
+++ app/components/Portfolio/index.js
@@ -13,10 +13,12 @@ import ins from './imgs/insuranceguru.png';
 const Portfolio = () => {
   const settings = {
     dots: true,
+    lazyLoad: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    initialSlide: 2,
   };
   return (
     <Section>
